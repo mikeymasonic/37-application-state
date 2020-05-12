9d2ce96d6aa9fcb1623b581c0007d88ed0e10587
Michael Romay <amerror@gmail.com>
2020-05-12 16:36:21 -0700

    finished up moodReducer

--- src/reducers/moodReducer.js
+++ src/reducers/moodReducer.js
@@ -1,4 +1,4 @@
-import { DRINK_COFFEE } from '../actions/moodActions';
+import { DRINK_COFFEE, EAT_SNACK, TAKE_NAP, STUDY } from '../actions/moodActions';
 
 const initialState = { 
   coffees: 0,
@@ -11,7 +11,13 @@ export default function reducer(state = initialState, action) {
   switch(action.type) {
     case DRINK_COFFEE:
       return { ...state, coffees: state.coffees + 1 };
-    
+    case EAT_SNACK:
+      return { ...state, snacks: state.snacks + 1 };
+    case TAKE_NAP:
+      return { ...state, naps: state.naps + 1 };
+    case STUDY:
+      return { ...state, study: state.study + 1 };
+    default:
+      return state;
   }
-
 }
