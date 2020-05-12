e1bfc79dbdafef09c60d1c45cbff3cbc8a2e4503
Michael Romay <amerror@gmail.com>
2020-05-12 16:30:39 -0700

    wrote test for STUDY moodAction

--- src/actions/moodActions.test.js
+++ src/actions/moodActions.test.js
@@ -1,4 +1,4 @@
-import { DRINK_COFFEE, drinkCoffee, EAT_SNACK, eatSnack, takeNap, TAKE_NAP } from './moodActions';
+import { DRINK_COFFEE, drinkCoffee, EAT_SNACK, eatSnack, takeNap, TAKE_NAP, study, STUDY } from './moodActions';
 
 
 describe('mood actions', () => {
@@ -14,10 +14,16 @@ describe('mood actions', () => {
     expect(action).toEqual({ type: EAT_SNACK });
   });
 
-  it('creats a take nap action', () => {
+  it('creates a take nap action', () => {
     const action = takeNap();
 
     expect(action).toEqual({ type: TAKE_NAP });
+  });
+
+  it('creates a study action', () => {
+    const action = study();
+
+    expect(action).toEqual({ type: STUDY });
   });
 
 });
